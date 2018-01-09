9beb8cac038d034298ad1673c7b7208afc0619db
JanSagat <erzhan.sagyt@gmail.com>
2018-01-09 09:57:50 +0600

    add parallax

--- src/template/common/common.js
+++ src/template/common/common.js
@@ -12,4 +12,56 @@ function clickHamburgerMenu() {
     }
 }
 
+function moveParallaxLayers(layer, windowScroll, speeds) {
+    let speed = speeds / .4;
+    let speedPercent = windowScroll / speed  + '%';
+    let transformString = `translate3d(0, ${speedPercent}, 0)`;
+
+    layer.style.transform = transformString;
+    layer.style.webkitTransform = transformString;
+}
+
+function scrollParallax() {
+    let parallaxContainer = document.querySelector('#parallax');
+    if(parallaxContainer) {
+        let layers = parallaxContainer.children;
+
+        window.addEventListener('scroll', function () {
+            let windowScroll = window.pageYOffset;
+
+            for(let i = 0; i < layers.length; i++) {
+                moveParallaxLayers(layers[i], windowScroll, i + 1);
+            }
+        });
+    }
+}
+
+function mouseParallax() {
+    let parallaxContainer = document.querySelector('#parallax_mouse');
+    if(parallaxContainer) {
+        let layers = parallaxContainer.children;
+
+        window.addEventListener('mousemove', function (e) {
+            let initialX = (window.innerWidth / 2) - e.pageX;
+            let initialY = (window.innerHeight / 2) - e.pageY;
+
+            [].slice.call(layers).forEach(function(layer, index) {
+                let
+                    divider = index / 200,
+                    positionX = initialX * divider,
+                    positionY = initialY * divider,
+                    bottomPosition = (window.innerHeight / 2) * divider,
+                    transformString = 'translate(' + positionX + 'px,' + positionY + 'px)',
+                    image = layer.firstElementChild;
+
+                layer.style.transform = transformString;
+                image.style.bottom = '-' + bottomPosition + 'px';
+            });
+
+        });
+    }
+}
+
 clickHamburgerMenu();
+scrollParallax();
+mouseParallax();
