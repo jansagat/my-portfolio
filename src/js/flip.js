52b859798c919b7cf9a74790036a0aaf1a9113e9
JanSagat <erzhan.sagyt@gmail.com>
2018-04-12 15:11:34 +0600

    fix flip

---
+++ src/js/flip.js
@@ -0,0 +1,17 @@
+(function () {
+    let auth = document.querySelector('#auth');
+    let toMain = document.querySelector('#toMain');
+    let flip = document.querySelector('#flip');
+
+    auth.addEventListener('click', function(e) {
+        e.preventDefault();
+        flip.classList.toggle('flip');
+        auth.style.display = 'none';
+    });
+
+    toMain.addEventListener('click', function(e) {
+        e.preventDefault();
+        flip.classList.toggle('flip');
+        auth.style.display = 'block';
+    });
+})();
--- src/template/pages/welcome/welcome.js
+++ src/template/pages/welcome/welcome.js
@@ -1,17 +1,2 @@
 import '../../../sass/main.scss';
-
-let auth = document.querySelector('#auth');
-let toMain = document.querySelector('#toMain');
-let flip = document.querySelector('#flip');
-
-auth.addEventListener('click', function(e) {
-    e.preventDefault();
-    flip.classList.toggle('flip');
-    auth.style.display = 'none';
-});
-
-toMain.addEventListener('click', function(e) {
-    e.preventDefault();
-    flip.classList.toggle('flip');
-    auth.style.display = 'block';
-});
+import '../../../js/flip'
