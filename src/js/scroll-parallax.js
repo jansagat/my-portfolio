3bdf7ffa1af1c6632b7127f348cf1a4ddf410123
JanSagat <erzhan.sagyt@gmail.com>
2018-04-20 12:39:47 +0600

    fix parallax

--- src/js/scroll-parallax.js
+++ src/js/scroll-parallax.js
@@ -1,11 +1,33 @@
 import Rellax from 'rellax';
 
 (function () {
-    var rellax = new Rellax('.rellax', {
+    let rellax = new Rellax('.rellax', {
         speed: -2,
         center: false,
         round: true,
         vertical: true,
         horizontal: false
+    });
+
+    window.addEventListener('DOMContentLoaded', function () {
+        if (window.innerWidth > 768) {
+            let images = document.getElementsByClassName('parallax__image');
+            for (let i = 0; i < images.length; i++) {
+                images[i].src = images[i].dataset.image
+            }
+        } else {
+            let header = document.querySelector('.header');
+            header.classList.add('header__bg');
+
+            rellax.destroy();
+        }
+    });
+
+    window.addEventListener('resize', function () {
+        if (window.innerWidth <= 768) {
+            rellax.destroy();
+        } else {
+            rellax.refresh();
+        }
     });
 })();
